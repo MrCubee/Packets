70365184c434a1a8b7b94dc28f15491158bed695
MrCubee <contact@mrcubee.fr>
2021-04-16 04:57:59 +0200

    [add] getLocale, getChatVisibility, isChatColor, getMainHand methods in PlayerUtils.

--- src/fr/mrcubee/bukkit/player/PlayerUtils.java
+++ src/fr/mrcubee/bukkit/player/PlayerUtils.java
@@ -2,6 +2,8 @@ package fr.mrcubee.bukkit.player;
 
 import com.mojang.authlib.GameProfile;
 import fr.mrcubee.bukkit.Versions;
+import fr.mrcubee.bukkit.setting.ChatVisibility;
+import fr.mrcubee.bukkit.setting.MainHand;
 import fr.mrcubee.util.Reflection;
 import org.bukkit.entity.Player;
 
@@ -39,5 +41,49 @@ public class PlayerUtils {
         if (clazz == null)
             return false;
         return (boolean) Reflection.executeStaticMethod(clazz, null, "setGameProfile", new Class[] {Player.class}, player);
+    }
+
+    public static String getLocale(Player player) {
+        Class<?> clazz;
+
+        if (player == null)
+            return null;
+        clazz = getCurrentClass();
+        if (clazz == null)
+            return null;
+        return (String) Reflection.executeStaticMethod(clazz, null, "getLocale", new Class[] {Player.class}, player);
+    }
+
+    public static ChatVisibility getChatVisibility(Player player) {
+        Class<?> clazz;
+
+        if (player == null)
+            return null;
+        clazz = getCurrentClass();
+        if (clazz == null)
+            return null;
+        return (ChatVisibility) Reflection.executeStaticMethod(clazz, null, "getChatVisibility", new Class[] {Player.class}, player);
+    }
+
+    public static Boolean isChatColor(Player player) {
+        Class<?> clazz;
+
+        if (player == null)
+            return false;
+        clazz = getCurrentClass();
+        if (clazz == null)
+            return false;
+        return (Boolean) Reflection.executeStaticMethod(clazz, null, "isChatColor", new Class[] {Player.class}, player);
+    }
+
+    public static MainHand getMainHand(Player player) {
+        Class<?> clazz;
+
+        if (player == null)
+            return null;
+        clazz = getCurrentClass();
+        if (clazz == null)
+            return null;
+        return (MainHand) Reflection.executeStaticMethod(clazz, null, "getMainHand", new Class[] {Player.class}, player);
     }
 }
--- src/fr/mrcubee/bukkit/player/v1_8_R3/PlayerUtils.java
+++ src/fr/mrcubee/bukkit/player/v1_8_R3/PlayerUtils.java
@@ -1,8 +1,11 @@
 package fr.mrcubee.bukkit.player.v1_8_R3;
 
 import com.mojang.authlib.GameProfile;
+import fr.mrcubee.bukkit.setting.ChatVisibility;
+import fr.mrcubee.bukkit.setting.MainHand;
 import fr.mrcubee.util.Reflection;
 import net.minecraft.server.v1_8_R3.EntityHuman;
+import net.minecraft.server.v1_8_R3.EntityPlayer;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
@@ -18,11 +21,66 @@ public class PlayerUtils {
     }
 
     public static boolean setGameProfile(Player player, GameProfile gameProfile) {
-        EntityHuman entityHuman;
+        EntityPlayer entityPlayer;
 
         if (player == null || gameProfile == null)
             return false;
-        entityHuman = ((CraftPlayer) player).getHandle();
-        return Reflection.setValue(entityHuman, "bH", gameProfile);
+        entityPlayer = ((CraftPlayer) player).getHandle();
+        if (entityPlayer == null)
+            return false;
+        return Reflection.setValue(entityPlayer, "bH", gameProfile);
+    }
+
+    public static String getLocale(Player player) {
+        EntityPlayer entityPlayer;
+
+        if (player == null)
+            return null;
+        entityPlayer = ((CraftPlayer) player).getHandle();
+        if (entityPlayer == null)
+            return null;
+        return entityPlayer.locale;
+    }
+
+    public static ChatVisibility getChatVisibility(Player player) {
+        EntityPlayer entityPlayer;
+        EntityHuman.EnumChatVisibility enumChatVisibility;
+
+        if (player == null)
+            return null;
+        entityPlayer = ((CraftPlayer) player).getHandle();
+        if (entityPlayer == null)
+            return null;
+        enumChatVisibility = entityPlayer.getChatFlags();
+        if (enumChatVisibility == null)
+            return null;
+        return ChatVisibility.getFromID(enumChatVisibility.a());
+    }
+
+    public static Boolean isChatColor(Player player) {
+        EntityPlayer entityPlayer;
+        byte value;
+
+        if (player == null)
+            return null;
+        entityPlayer = ((CraftPlayer) player).getHandle();
+        if (entityPlayer == null)
+            return null;
+        return (Boolean) Reflection.getValue(entityPlayer, "bS");
+    }
+
+    public static MainHand getMainHand(Player player) {
+        EntityPlayer entityPlayer;
+        byte value;
+
+        if (player == null)
+            return null;
+        entityPlayer = ((CraftPlayer) player).getHandle();
+        if (entityPlayer == null)
+            return null;
+        value = entityPlayer.getDataWatcher().getByte(10);
+        if (value < 0 || value > 1)
+            return null;
+        return MainHand.values()[value];
     }
 }
